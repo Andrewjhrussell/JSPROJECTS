0f61abbab164ca512a4bdde50d2d62098041e059
Andrewjhrussell <andrewjhrussell@gmail.com>
2020-10-13 18:12:24 -0400

    JS Code

---
+++ home.js
@@ -0,0 +1,15 @@
+console.log('PREPARE TO FIGHT THE DRAGON KING!!!!!!!');
+alert("CLICK OK NOW SO YOU DONT GET BURNED BY FIRE!!!!")
+
+var a = 'DRAGON';
+console.log(a);
+
+var someNumber = 45;
+console.log(someNumber);
+
+//Manipulate DOM with JS
+
+
+var age = prompt("What is your age?");
+
+document.getElementById('someText').innerHTML = age;
